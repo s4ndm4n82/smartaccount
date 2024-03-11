83b88f8f59ad509de7184fee3531a86ade9a0036
Sandaruwan Samaraweera <sandaruwan12@gmail.com>
2024-03-11 16:18:55 +0530

    Added UserRoles table CS file.

---
+++ SmartAccount/Models/TableUserRoles.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace SmartAccount.Models
+{
+    public class TableUserRoles
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string RoldeName { get; set; }
+
+        [Required]
+        public JsonDocument RoleAcess { get; set; }
+
+        [Required]
+        public bool RoleActive { get; set; }
+    }
+}
